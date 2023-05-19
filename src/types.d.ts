7dbe04e9ccff2f7a55472d090f13aba3d80cb487
joe <joesriprajittichai@gmail.com>
2023-05-19 15:31:18 -0400

    finished tutorial, user auth and starter html working

---
+++ src/types.d.ts
@@ -0,0 +1,52 @@
+interface UserProfile {
+    country: string;
+    display_name: string;
+    email: string;
+    explicit_content: {
+        filter_enabled: boolean,
+        filter_locked: boolean
+    },
+    external_urls: { spotify: string; };
+    followers: { href: string; total: number; };
+    href: string;
+    id: string;
+    images: Image[];
+    product: string;
+    type: string;
+    uri: string;
+}
+
+interface Image {
+    url: string;
+    height: number;
+    width: number;
+}
+
+interface UserPlaylists {
+    href: string;
+    limit: number;
+    next: string;
+    offset: number;
+    previous: string;
+    total: number;
+    items: SimplifiedPlaylist[];
+}
+
+interface SimplifiedPlaylist {
+    collaborative: boolean;
+    description: string;
+    external_urls: { spotify: string; };
+    href: string;
+    id: string;
+    images: Image[];
+    name: string;
+    owner: UserProfile;
+    public: boolean;
+    snapshot_id: string;
+    tracks: {
+        href: string,
+        total: number
+    };
+    type: string;
+    uri: string;
+}
