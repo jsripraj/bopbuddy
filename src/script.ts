e9c38a98142ff4cb40f66f63708a2434caa360ae
joe <joesriprajittichai@gmail.com>
2023-05-20 17:48:17 -0400

    test commit

--- src/script.ts
+++ src/script.ts
@@ -69,7 +69,7 @@ export async function getAccessToken(clientId: string, code: string): Promise<st
 }
 
 async function fetchProfile(token: string): Promise<UserProfile> {
-    const result = await fetch("https://api.spotify.com/v1/me", {
+    let result = await fetch("https://api.spotify.com/v1/me", {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
 
